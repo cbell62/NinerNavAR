6f401af8cee4b2343135d2f6470d06ae4036cea4
Jason Fomuke <jason.fomuke@gmail.com>
2021-05-02 18:37:03 -0400

    First commit

---
+++ logic.js
@@ -0,0 +1,36 @@
+
+// Hide this object
+//var hideMe = document.querySelector("#allosaur");
+//hideMe.setAttribute("visible",false);
+// document.getElementById("pinkSphere").setAttribute(visible,false);
+
+
+
+
+
+/*
+document.querySelector('#myElParentId').addEventListener('mouseenter',myEventHandler); 
+
+myEventHandler: function (evt) {
+  let myEl = document.querySelector("#hand-overview-chart");
+  myEl.setAttribute("visible","true");
+}
+*/
+
+
+// console.log("ConsoleLog is working (☞ﾟヮﾟ)☞");
+
+// document.getElementById("pinkSphere").setAttribute("visible",false);
+
+// console.log(String(document.getElementById("pinkSphere")));
+/*
+function hideMe(objectID) 
+{
+    document.getElementById(objectID).setAttribute(visible,false);
+    console.log(objectID + " has been hidden");
+    return;
+}
+*/
+
+
+
